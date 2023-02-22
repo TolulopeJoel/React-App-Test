d6c72ca420f07ebdc2a65a1bde5d9d3d1ce72d49
Tolu Joel <toluisjoel@gmail.com>
2023-02-22 18:31:36 +0100

    Changed the way data was displayed

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -33,10 +33,10 @@ class AllTasks extends Component {
             {tasks.map(task => (
               <a href={`/tasks/${task.id}`}>
                 <tr key={task.id}>
-                  <td className="task-title"><a href={`/tasks/${task.id}`}>{task.title}</a></td>
+                  <td className="task-title"><a href={`/tasks/${task.id}`}>{task.name}</a></td>
                   <td>{task.description.substring(0, 20)}...</td>
-                  <td>{task.due_date}</td>
-                  <td>{task.assignee}</td>
+                  <td>{Date(task.due_date)}</td>
+                  <td>{task.assignee.username}</td>
                   <td className={`status ${task.status}`}>{task.status}</td>
                 </tr>
               </a>
@@ -49,11 +49,6 @@ class AllTasks extends Component {
         {/* Include the contents of the "calendar.html" template directly in this component */}
         {/* Replace any Django template variables with JavaScript variables or props */}
         {/* Example: {% url 'task_detail' task.id %} becomes `/tasks/${task.id}` */}
-        <div dangerouslySetInnerHTML={{
-          __html: `
-          <h2>Calendar View</h2>
-          ... contents of calendar.html template go here ...
-        `}} />
       </div>
     );
   }
