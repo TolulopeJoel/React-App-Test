4f81a86fa98b5d2475b3040f7adc0f94f9084750
Tolu Joel <toluisjoel@gmail.com>
2023-02-22 21:05:17 +0100

    Removed redundant code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TaskDetail from './components/TaskDetail.js';
 import AllTasks from './components/AllTasks.js';
-
-
-// function TaskDetailWrapper(props) {
-//   const taskId = useParams()
-//   console.log(taskId)
-//   const task = tasks.find(task => task.id === taskId);
-//   return task ? <TaskDetail {...task} {...props} /> : <div>Task not found</div>;
-// }
 
 
 function App() {
