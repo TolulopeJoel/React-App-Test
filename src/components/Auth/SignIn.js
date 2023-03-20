77e5e0a3502cdefb1cef1f128d847a7d637f3712
Tolu Joel <toluisjoel@gmail.com>
2023-03-20 18:01:36 +0100

    added styling to signin

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,6 +1,28 @@
+import api from "../api.js";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 import React, { useState } from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
-import api from "../Api";
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'© Pass Store'} {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 function SignIn() {
   const [username, setUsername] = useState("");
@@ -24,25 +46,40 @@ function SignIn() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-      <button type="submit">Sign In</button>
-    </form>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField margin="normal" required fullWidth label="Username" name="text" autoFocus value={username} onChange={(event) => setUsername(event.target.value)} />
+            <TextField margin="normal" required fullWidth label="Password" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} > Sign In </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* <Link href="#" variant="body2"> Forgot password? </Link> */}
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2"> {"Don't have an account? Sign Up"} </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
