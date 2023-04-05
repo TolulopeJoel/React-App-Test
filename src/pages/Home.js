b6f9180ea1567de320fffed684e5f17eaecfba49
Tolu Joel <toluisjoel@gmail.com>
2023-04-05 23:58:49 +0100

    More styling to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,46 +8,43 @@ export default function Home() {
             <Navbar />
             <div className="banner bg-light text-center">
                 <div className="container">
-                    <h1 className="fw-bold py-3">Search engine optimisation &<br />Marketing.</h1>
-                    <h6 className="text-muted pb-3">Simple is a simple template with a creative design that solves all your marketing and SEO queries.</h6>
+                    <h1 className="fw-bold py-3">Collaborate and Meet up With<br />Deadlines in Realtime.</h1>
+                    <h6 className="text-muted pb-3">RemoteCollaborate keeps your team more organized. And meet up with task deadlines. </h6>
                     <div>
-                        <button className="btn opacity-75 mx-1 btn-success rounded-0">Get started</button>
-                        <button className="btn opacity-75 mx-1 btn-danger rounded-0">Learn more</button>
+                        <a href="/signup" className="btn opacity-75 mx-1 btn-success rounded-0 px-3">Get started</a>
+                        <a href="/login" className="btn opacity-75 mx-1 border border-danger rounded-0 px-3">Sign In</a>
                     </div>
                     <img src={require('../assets/images/hero-image.svg')} alt="" className="img-fluid" />
                 </div>
             </div>
 
-            <div className="container">
-                <section>
+            <div className="container my-5">
+                <section className="my-5">
                     <div className="text-center">
-                        <h2>How does it works</h2>
-                        <h6 className="text-muted">One theme that serves as an easy-to-use operational toolkit<br />that meets customer's needs.</h6>
+                        <h2>How does it work?</h2>
+                        <h6 className="text-muted">Avoid messy deadlines. Track tasks progress and<br />meet customer's needs.</h6>
                     </div>
 
-                    <div className="d-md-flex justify-content-between">
+                    <div className="d-md-flex justify-content-between my-5">
                         <div className="d-flex justify-content-start">
-                            <div className="features-width">
-                                <img src="images/Group12.svg" alt="" className="img-icons" />
-                                <h5 className="py-3">Speed<br />Optimisation</h5>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-                                <a href="#"><p className="readmore-link">Readmore</p></a>
+                            <div className='w-50'>
+                                <img src={require('../assets/images/Group12.svg')} alt="" />
+                                <h5 className="py-3">Work in<br />Teams</h5>
+                                <p className="text-muted">Collaboration made easy by working in various teams.</p>
                             </div>
                         </div>
                         <div className="d-flex justify-content-center">
-                            <div className="features-width">
-                                <img src="images/Group7.svg" alt="" className="img-icons" />
-                                <h5 className="py-3">SEO and<br />Backlinks</h5>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-                                <a href="#"><p className="readmore-link">Readmore</p></a>
+                            <div className='w-50'>
+                                <img src={require('../assets/images/Group7.svg')} alt="" />
+                                <h5 className="py-3">Realtime Progress</h5>
+                                <p className="text-muted">Get updates on task and teamates progress in realtime.</p>
                             </div>
                         </div>
                         <div className="d-flex justify-content-end">
-                            <div className="features-width">
-                                <img src="images/Group5.svg" alt="" className="img-icons" />
-                                <h5 className="py-3">Content<br />Marketing</h5>
-                                <p className="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-                                <a href="#"><p className="readmore-link">Readmore</p></a>
+                            <div className='w-50'>
+                                <img src={require('../assets/images/Group5.svg')} alt="" />
+                                <h5 className="py-3">Track Tasks<br />Progress</h5>
+                                <p className="text-muted">View tasks progress and know if you are ahead or behind schedule.</p>
                             </div>
                         </div>
                     </div>
