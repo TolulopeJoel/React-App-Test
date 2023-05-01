f21d8cb261f43e3a402006f45e7dfc57e03f7729
Tolu Joel <toluisjoel@gmail.com>
2023-05-01 14:13:45 +0100

    Made table responsive for task list in team detail

--- src/pages/team/TeamDetail.js
+++ src/pages/team/TeamDetail.js
@@ -39,39 +39,46 @@ export default function TeamDetail() {
             <div className="container">
                 <h1 className="my-4">{team.name}</h1>
 
-                <table className="table table-bordered table-hover my-5">
-                    <thead>
-                        <tr>
-                            <th>Task</th>
-                            <th>Description</th>
-                            <th>Due Date</th>
-                            <th>Assignees</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
+                <div className="table-responsive">
+                    <table className="table table-bordered table-hover my-5">
+                        <thead>
+                            <tr>
+                                <th>Task</th>
+                                <th>Description</th>
+                                <th>Due Date</th>
+                                <th>Assignees</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
 
-                    {team.tasks.length > 0 &&
-                        <tbody>
-                            {team.tasks && team.tasks.map(task => (
-                                <tr key={task.id}>
-                                    <td className="task-title"><a href={`/tasks/${task.id}`}>{task.name}</a></td>
-                                    <td>{task.description.substring(0, 20)}...</td>
-                                    <td>{new Date(task.due_date).toDateString()}</td>
-                                    <td className="task-assignee">{task.assignees && task.assignees.map(assignee => (
-                                        <span>{assignee.username}, </span>
-                                    ))}</td>
-                                    <td className={`status ${task.status}`}>{task.status}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    }
+                        {team.tasks.length > 0 &&
+                            <tbody>
+                                {team.tasks && team.tasks.map(task => (
+                                    <tr key={task.id}>
+                                        <td className="task-title"><a href={`/tasks/${task.id}`}>{task.name}</a></td>
+                                        <td>{task.description.substring(0, 20)}...</td>
+                                        <td>{new Date(task.due_date).toDateString()}</td>
+                                        <td>
+                                            {task.assignees && task.assignees.map((assignee, index) => (
+                                                <span key={assignee.id} className="p-1">
+                                                    {assignee.username}
+                                                    {index < task.assignees.length - 1 ? ", " : "."}
+                                                </span>
+                                            ))}
+                                        </td>
+                                        <td className={`status ${task.status}`}>{task.status}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        }
 
-                    {team.tasks.length === 0 &&
-                        <div>
-                            <h4>There are no tasks for this team.<br />Tell the assigner to add one. Or give you access to do so.</h4>
-                        </div>
-                    }
-                </table>
+                        {team.tasks.length === 0 &&
+                            <div>
+                                <h4>There are no tasks for this team.<br />Tell the assigner to add one. Or give you access to do so.</h4>
+                            </div>
+                        }
+                    </table>
+                </div>
 
                 <a href={`/teams/${team.id}/new-task`} className="create-button">Create New Task</a>
             </div>
